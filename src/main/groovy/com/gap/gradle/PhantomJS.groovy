348e2092e7496ee9cb8ba4dc9749f5a1a6364541
er0t0c2 <er0t0c2@64f59038-03c2-4741-9c8b-d8a2384221e3>
2013-07-18 21:44:17 +0000

    ITSCMSPL-000 [Mrak] fixing broken PhantomJS.groovy compilation errors
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1204 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/PhantomJS.groovy
+++ src/main/groovy/com/gap/gradle/PhantomJS.groovy
@@ -3,40 +3,45 @@ package com.gap.gradle
 import java.util.Map;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.JavaExec;
+import org.gradle.api.tasks.Copy;
+import org.gradle.api.tasks.Exec;
 import org.gradle.api.Project;
 
 class PhantomJS extends DefaultTask {
-  def args
-  def version
-  def final String phantomjsBin = "${buildDir}/bin"
+  def Iterable<?> args
+  def String version = '1.9.1'
+  def final String phantomjsBin = "${project.buildDir}/bin/phantomjs"
 
   @TaskAction
   def run() {
+    def String os = System.properties['os.name'].toLowerCase()
+    def String arch = System.properties['os.arch'].toLowerCase()
+
+    def String qualifier
     if (os.indexOf('linux') >= 0) {
         if (arch.indexOf('64') >= 0) {
-            qualifier "linux-x86_64"
+            qualifier = "linux-x86_64"
         } else {
-            qualifier "linux-i686"
+            qualifier = "linux-i686"
         }
     } else if (os.indexOf('mac') >= 0) {
-        qualifier "macosx"
+        qualifier = "macosx"
     } else {
-        qualifier "windows"
+        qualifier = "windows"
     }
 
     project.configurations { binaries }
     project.dependencies.binaries "org.phantomjs:phantomjs:${version}:${qualifier}@zip"
     project.configurations.binaries.each { file ->
-        copy {
-            from zipTree(file)
-            into "${buildDir}/bin"
+        project.copy {
+            from project.zipTree(file)
+            into "${project.buildDir}/bin"
         }
     }
 
     project.exec {
         executable = phantomjsBin
-        args = args
+        args = this.args
     }
   }
 }
