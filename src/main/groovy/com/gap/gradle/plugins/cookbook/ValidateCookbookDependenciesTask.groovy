08491efbb03e4f07ccffdde7e8249e8d09baa56f
lo5u3i8 <lo5u3i8@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-01 00:05:50 +0000

    ITCI-920 [Logan] Some refactoring of range pattern
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1668 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/plugins/cookbook/ValidateCookbookDependenciesTask.groovy
+++ src/main/groovy/com/gap/gradle/plugins/cookbook/ValidateCookbookDependenciesTask.groovy
@@ -6,7 +6,7 @@ import org.gradle.api.Project
 
 class ValidateCookbookDependenciesTask {
 
-    private static final def ranges = [ "<", "<=", ">=", "~>", ">" ].each { t -> quote(t) }.join("|")
+    private static final def ranges = /[${[ "<", "<=", ">=", "~>", ">" ].each { t -> quote(t) }.join("|")}]/
 
     Project project
 
@@ -22,7 +22,7 @@ class ValidateCookbookDependenciesTask {
                     throw new UnpinnedDependencyException(
                         "Cookbook dependency '" + entry.key + "' has no version pinned"
                     )
-                } else if (entry.value =~ /[$ranges]/) {
+                } else if (entry.value =~ ranges) {
                     throw new UnpinnedDependencyException(
                         "Cookbook dependency '" + entry.key + "' version '" + entry.value + "' is not pinned"
                     )
