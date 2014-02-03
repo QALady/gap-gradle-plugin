eee7e8a5f2d0ec5691c2f6a46cd4367cea4c19dd
lo5u3i8 <lo5u3i8@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-03 18:07:53 +0000

    ITCI-920 [Logan] Restricting visibility of ChefConfig#metadata
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1672 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/plugins/cookbook/ChefConfig.groovy
+++ src/main/groovy/com/gap/gradle/plugins/cookbook/ChefConfig.groovy
@@ -1,8 +1,16 @@
 package com.gap.gradle.plugins.cookbook
 
+import groovy.transform.PackageScope
+
 class ChefConfig {
+
     def cookbookName
     def environment = "tdev"
     def cookbookDir = "."
+
+    /**
+     * Metadata should not be user-configurable: it is populated by task {@link GenerateCookbookMetadataTask}.
+     */
+    @PackageScope
     def metadata
 }
