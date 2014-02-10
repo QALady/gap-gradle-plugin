54f3667d6e0cbcc8771fea4a65b877411e7ca0d0
vi4p7u5 <vi4p7u5@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-10 19:14:40 +0000

    ITCI-925 [VJ] Adding coverage to promote chef objects task
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1739 64f59038-03c2-4741-9c8b-d8a2384221e3

---
+++ src/test/groovy/com/gap/gradle/plugins/cookbook/PromoteChefObjectsToServerTaskTest.groovy
@@ -0,0 +1,124 @@
+package com.gap.gradle.plugins.cookbook
+
+import static junit.framework.Assert.assertFalse
+import static net.sf.ezmorph.test.ArrayAssertions.assertEquals
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.junit.internal.matchers.StringContains.containsString
+
+import com.gap.gradle.jenkins.JenkinsRunner
+import groovy.mock.interceptor.MockFor
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Test
+
+class PromoteChefObjectsToServerTaskTest {
+
+    Project project
+    Task promoteChefObjectsTask
+    def mockJenkinsRunner
+    def mockCookbookUtil
+
+    @Before
+    void setUp (){
+        project = ProjectBuilder.builder().build()
+        project.apply plugin: 'gapcookbook'
+        promoteChefObjectsTask = project.tasks.findByName('promoteChefObjectsToServer')
+
+        mockJenkinsRunner = new MockFor(JenkinsRunner)
+        //mockCookbookUtil = new MockFor(CookbookUtil)
+        //project.chef.metadata = [ name: "myapp", version: "1.1.13" ]
+    }
+
+    @Test
+    void shouldThrowException_whenJenkinsServerUrlIsNotConfigured(){
+        assertThrowsExceptionWithMessage("No jenkins url configured", {promoteChefObjectsTask.execute()})
+    }
+
+    @Test
+    void shouldThrowException_whenJenkinsUserNameIsNotConfigured(){
+        project.jenkins.knifeServerUrl = "testserver"
+        assertThrowsExceptionWithMessage("No jenkins user configured", {promoteChefObjectsTask.execute()})
+    }
+
+    @Test
+    void shouldThrowException_whenJenkinsApiTokenIsNotConfigured(){
+        project.jenkins.knifeServerUrl = "jenkins"
+        project.jenkins.knifeUser = "jenkins_user"
+        assertThrowsExceptionWithMessage("No jenkins auth-token configured", {promoteChefObjectsTask.execute()})
+    }
+
+    @Test
+    void shouldThrowException_whenJenkinsJobNameIsNotConfigured(){
+        project.jenkins.knifeServerUrl = "jenkins"
+        project.jenkins.knifeUser = "jenkins_user"
+        project.jenkins.knifeAuthToken = "jenkins_auth"
+        assertThrowsExceptionWithMessage("No jenkins jobName configured", {promoteChefObjectsTask.execute()})
+    }
+
+//    @Test
+    void shouldTriggerPromoteChefObjectsJob_whenAllParametersArePassedr(){
+//        setupTaskProperties()
+//        mockCookbookUploader.demand.upload { cookbook, env ->
+//            assertEquals("myapp", cookbook)
+//            assertEquals("local", env)
+//        }
+//        mockCookbookUtil.demand.doesCookbookExist { return false }
+//
+//        mockCookbookUtil.use {
+//           mockCookbookUploader.use {
+//               promoteChefObjectsTask.execute()
+//           }
+//        }
+    }
+
+//    @Test
+    void shouldGetCookbookNameFromMetadata_whenCookbookNameIsNotProvided(){
+//        setupTaskProperties()
+//
+//        mockCookbookUtil.demand.doesCookbookExist {return false}
+//        mockCookbookUploader.demand.upload { cookbook, env ->
+//            assertEquals("myapp", cookbook)
+//            assertEquals("local", env)
+//        }
+//
+//        mockCookbookUtil.use {
+//            mockCookbookUploader.use {
+//                publishCookbookTask.execute()
+//            }
+//        }
+    }
+
+//    @Test
+    void shouldNotTriggerUpload_whenCookbookVersionAlreadyExistsInChef(){
+//        setupTaskProperties()
+//
+//        mockCookbookUtil.demand.doesCookbookExist { return true }
+//
+//        mockCookbookUploader.ignore.upload {}
+//
+//        mockCookbookUploader.use{
+//            mockCookbookUtil.use{
+//                publishCookbookTask.execute()
+//            }
+//        }
+    }
+
+    private void setupTaskProperties() {
+        project.jenkins.knifeServerUrl = "jenkins"
+        project.jenkins.knifeUser = "jenkins_user"
+        project.jenkins.knifeAuthToken = "jenkins_password"
+        project.chef.environment = "local"
+    }
+
+    void assertThrowsExceptionWithMessage(expectedMessage, Closure closure){
+        try{
+            closure()
+            assertFalse("Expected exception with message '${expectedMessage} but got none", true)
+        }
+        catch(Exception ex){
+            assertThat(ex.dump(), containsString(expectedMessage))
+        }
+    }
+}
