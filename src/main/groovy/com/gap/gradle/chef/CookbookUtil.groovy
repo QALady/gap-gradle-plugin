67c1ba331f02e82fb08f4cf7410e20fcbe3b1425
vi4p7u5 <vi4p7u5@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-14 23:49:45 +0000

    ITCI-000 [Vijay] Logging cookbook info for debugging
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1821 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/chef/CookbookUtil.groovy
+++ src/main/groovy/com/gap/gradle/chef/CookbookUtil.groovy
@@ -2,8 +2,12 @@ package com.gap.gradle.chef
 
 import com.gap.gradle.utils.ShellCommand
 import groovy.json.JsonSlurper
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 class CookbookUtil {
+
+    private Log log = LogFactory.getLog(CookbookUtil)
 
     def metadataFrom(cookbookDir = ".") {
         new ShellCommand().execute("knife cookbook metadata from file ${"${cookbookDir}/metadata.rb"}")
@@ -13,10 +17,10 @@ class CookbookUtil {
     def doesCookbookExist(cookbookMetadata) {
         try {
             def output = new ShellCommand().execute("knife cookbook show ${cookbookMetadata.name}")
+            log.info("Output: ${output} - ${cookbookMetadata.version}")
             return output.contains(" ${cookbookMetadata.version} ")
         }
-        catch(Exception)
-        {
+        catch(Exception) {
             return false
         }
     }
