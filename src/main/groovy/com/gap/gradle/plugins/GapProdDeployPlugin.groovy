97f420726fb2f0bdf556094fbea00df20b33de2a
kr8s8k9 <kr8s8k9@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-14 23:31:18 +0000

    ITCI-000 [Krishna, Logan] adding poor-mans logging to debug
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1820 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/plugins/GapProdDeployPlugin.groovy
+++ src/main/groovy/com/gap/gradle/plugins/GapProdDeployPlugin.groovy
@@ -3,7 +3,6 @@ package com.gap.gradle.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-import com.gap.gradle.ProdDeployConfig
 import com.gap.gradle.ProdDeployParameterConfig
 import com.gap.gradle.plugins.cookbook.JenkinsConfig
 import com.gap.gradle.tasks.DeployToProductionTask
@@ -20,22 +19,20 @@ class GapProdDeployPlugin implements Plugin<Project>{
 		loadJenkinsConfig(project)
 		loadProdDeployConfig(project)
 
-		project.task('prepareToPromote') {
-			doLast {
-				new PrepareToPromoteToProductionTask(project).execute()
-			}
+		project.task('prepareToPromote') << {
+			println "preparing to promote"
+			new PrepareToPromoteToProductionTask(project).execute()
 		}
 
-		project.task('promoteToProduction', dependsOn: 'prepareToPromote') {
-			doLast {
-				new PromoteToProductionTask(project).execute()
-			}
+		project.task('promoteToProduction', dependsOn: 'prepareToPromote') << {
+			println "promoting to production"
+			new PromoteToProductionTask(project).execute()
 		}
 
-		project.task('deployToProduction', dependsOn: 'promoteToProduction') {
-			doLast {
-				new DeployToProductionTask(project).execute()
-			}
+		project.task('deployToProduction', dependsOn: 'promoteToProduction') << {
+			println "deploying to production"
+			new DeployToProductionTask(project).execute()
+			println "WOO! done (:"
 		}
     }
 	
