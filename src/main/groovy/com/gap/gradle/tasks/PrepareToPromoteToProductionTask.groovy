1b2de9da03a0c4f2bda92996aac0cd6c6b439cf7
kr8s8k9 <kr8s8k9@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-17 19:42:37 +0000

    ITCI-949 [krishna, Logan] Adding required constructor to the task.
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1839 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/tasks/PrepareToPromoteToProductionTask.groovy
+++ src/main/groovy/com/gap/gradle/tasks/PrepareToPromoteToProductionTask.groovy
@@ -1,7 +1,17 @@
 package com.gap.gradle.tasks
 
-class PrepareToPromoteToProductionTask {
+import org.gradle.api.Project
+
+class PrepareToPromoteToProductionTask extends WatchmenTask {
+
+	PrepareToPromoteToProductionTask(Project project) {
+		super(project)
+		this.project = project
+	}
+
 	void execute() {
 		// do something
+		// TODO: here we need to integrate the piece of calling the Git upload task to 
+		// update the Cookbook sha1ID into Berksfile.prod.
 	}
 }
