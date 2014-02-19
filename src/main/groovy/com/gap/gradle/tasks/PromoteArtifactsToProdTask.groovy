60e752138281b958964c41415f808338e88323af
ga5h0w4 <ga5h0w4@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-19 05:03:51 +0000

    ITCI-946 [ganesh] adding tests for validating input
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1875 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/tasks/PromoteArtifactsToProdTask.groovy
+++ src/main/groovy/com/gap/gradle/tasks/PromoteArtifactsToProdTask.groovy
@@ -1,14 +1,26 @@
 package com.gap.gradle.tasks
 
-class PromoteArtifactsToProdTask {
+import com.gap.pipeline.tasks.WatchmenTask
+import com.gap.pipeline.tasks.annotations.Require
+import com.gap.pipeline.tasks.annotations.RequiredParameters
+
+@RequiredParameters([
+@Require(parameter = 'artifactCoordinates', description = 'location to download artifacts from'),
+@Require(parameter = 'artifactConfiguration', description = 'ivy configuration to download artifacts from'),
+@Require(parameter = 'toArtifactCoordinates', description = 'ivy configuration to upload the artifacts')
+])
+
+class PromoteArtifactsToProdTask extends WatchmenTask {
 
     private def project
 
     PromoteArtifactsToProdTask (project){
+        super(project)
         this.project = project
     }
 
     def execute(){
+        validate()
         def downloadDir = "${project.rootDir}/downloads/"
         new File(downloadDir).mkdirs()
         project.tasks.findByName('downloadArtifacts').execute()
--- src/test/groovy/com/gap/gradle/tasks/PromoteArtifactsToProdTaskTest.groovy
+++ src/test/groovy/com/gap/gradle/tasks/PromoteArtifactsToProdTaskTest.groovy
@@ -2,29 +2,37 @@ package com.gap.gradle.tasks
 import static junit.framework.Assert.assertTrue
 
 import com.gap.gradle.utils.ShellCommand
+import com.gap.pipeline.exception.MissingParameterException
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
-import org.gradle.tooling.model.GradleTask
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.rules.TemporaryFolder
 
 class PromoteArtifactsToProdTaskTest {
     private Project project
-    private GradleTask gradleTask
+    private PromoteArtifactsToProdTask task
 
     @Before
     void setUp() {
         project = ProjectBuilder.builder().withProjectDir(new File(temporaryFolder.root.path)).build()
         project.apply plugin: 'gapproddeploy'
         project.artifactCoordinates = 'com.gap.sandbox:testDownload:123'
+        project.toArtifactCoordinates = 'com.gap.sandbox:testDownload'
+
         project.destination = 'downloads'
         project.artifactConfiguration = 'archives'
+        task = new PromoteArtifactsToProdTask(project)
     }
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder()
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none()
 
     @Test
     void shouldSuccessfullyPromoteArtifacts() {
@@ -37,12 +45,39 @@ class PromoteArtifactsToProdTaskTest {
         project.task('uploadBuildArtifacts') << {
             uploadArtifactsWasCalled = true
         }
-        def task = new PromoteArtifactsToProdTask(project)
         task.execute()
 
         ["${project.rootDir}/build/artifacts/file1.txt", "${project.rootDir}/build/artifacts/file2.txt"].each{ file ->
-            assertTrue("Cannot find file with name ${file}", new File(file).exists())
+            assertTrue("Cannot find ${file}", new File(file).exists())
         }
         assertTrue("uploadBuildArtifacts task was not invoked.", uploadArtifactsWasCalled)
     }
+
+    @Test
+    @Ignore
+    void shouldThrownException_whenArtifactCoordinatesAreNotProvided() {
+        exception.expect(MissingParameterException)
+        exception.expectMessage("Missing required parameter: 'artifactCoordinates'")
+        project.artifactCoordinates=null
+        task.validate()
+    }
+
+    @Test
+    @Ignore
+    void shouldThrownException_whenArtifactConfigurationNotProvided() {
+        exception.expect(MissingParameterException)
+        exception.expectMessage("Missing required parameter: 'artifactConfiguration'")
+        project.artifactConfiguration=null
+        task.validate()
+    }
+
+    @Test
+    @Ignore
+    void shouldThrownException_whenDestinationArtifactCoordinatesAreNotProvided() {
+        exception.expect(MissingParameterException)
+        exception.expectMessage("Missing required parameter: 'toArtifactCoordinates'")
+        project.toArtifactCoordinates=null
+        task.validate()
+    }
+
 }
