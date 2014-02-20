999bcde02d0bbd1a662c6be87dedc8fb88404edd
kr8s8k9 <kr8s8k9@64f59038-03c2-4741-9c8b-d8a2384221e3>
2014-02-20 02:34:29 +0000

    ITCI-949 [Emily, Krishna] converting isRpm to isRPM. removed logging.
    git-svn-id: http://svn.gap.dev/svn/ci/trunk/gradle-plugins/gap-gradle-plugin@1910 64f59038-03c2-4741-9c8b-d8a2384221e3

--- src/main/groovy/com/gap/gradle/tasks/PrepareToPromoteToProductionTask.groovy
+++ src/main/groovy/com/gap/gradle/tasks/PrepareToPromoteToProductionTask.groovy
@@ -3,10 +3,9 @@ package com.gap.gradle.tasks
 import org.gradle.api.Project
 
 import com.gap.pipeline.tasks.WatchmenTask
-import com.sun.org.apache.commons.logging.LogFactory
 
 class PrepareToPromoteToProductionTask extends WatchmenTask {
-	def log = LogFactory.getLog(PrepareToPromoteToProductionTask)
+//	def log = LogFactory.getLog(PrepareToPromoteToProductionTask)
 
 	PrepareToPromoteToProductionTask(Project project) {
 		super(project)
@@ -17,9 +16,9 @@ class PrepareToPromoteToProductionTask extends WatchmenTask {
 		//project.tasks.findByName('promoteCookbookBerksfile').execute()
 
 		// task: promote RPMs to prod if the artifact is rpms
-		log.info("checking if artifact is RPM: ${project.prodDeploy.isRPM}")
+//		log.info("checking if artifact is RPM: ${project.prodDeploy.isRPM}")
         if(project.prodDeploy.isRPM){
-			log.info("executing promoteRpm task")
+//			log.info("executing promoteRpm task")
             project.tasks.findByName('promoteRpm').execute()
         }
 	}
