45bddd3c1e549f5223ed226e9b4710c4aabc5e39
krishnarangavajhala <krishnarangavajhala@172.17.100.174>
2014-05-26 14:33:18 -0700

    ITCI-17 [Krishna, Claudio, Alberto] Adding GapAthena plugin test

---
+++ src/test/groovy/com/gap/gradle/plugins/GapAthenaPluginTest.groovy
@@ -0,0 +1,46 @@
+package com.gap.gradle.plugins
+
+import static org.hamcrest.Matchers.*
+import static org.junit.Assert.*
+
+import org.apache.commons.io.FileUtils
+import org.apache.tools.ant.types.Path
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class GapAthenaPluginTest {
+
+	private Project project
+	private static final String sampleJarFile = "com/gap/gradle/resources/sample-ant-classpath-import-jar"
+	
+   @Rule
+   public TemporaryFolder tempFolder = new TemporaryFolder();
+
+   @Test
+    void testAntPathInjection() {
+        project = ProjectBuilder.builder().build()
+        project.antDepConf = "athenadeps"
+        project.antPathName = "build.classpath"
+        project.configurations {
+            athenadeps
+        }
+        project.dependencies {
+            athenadeps project.files(sampleJarFile)
+        }
+		
+		def buildFile = tempFolder.newFile("test-build.xml")
+		FileUtils.writeStringToFile(buildFile, '<project default="development" basedir="."><path id="build.classpath"></path><target name="development"></target></project>')
+ 
+		project.ant.importBuild  buildFile
+
+		project.apply plugin: 'gapant'
+		
+		Path actualPath = project.ant.getReferences().get("build.classpath")
+		assertNotNull(actualPath)
+		assertTrue(actualPath.list()[0].contains(sampleJarFile))
+
+    }
+}
