4ddf4b6fd3855901a67212046537e19fb129d16f
Vijay Bandari <vijay_bandari@gap.com>
2014-05-27 14:04:32 -0700

    ITCI-000 [Vijay] Updating artifactory credentials

--- src/main/groovy/com/gap/pipeline/IvyConfig.groovy
+++ src/main/groovy/com/gap/pipeline/IvyConfig.groovy
@@ -2,7 +2,7 @@ package com.gap.pipeline
 
 class IvyConfig {
     def url = "http://artifactory.gapinc.dev/artifactory/local-non-prod"
-    def userName = "admin"
-    def password = "password"
+    def userName = "ec-build"
+    def password = "EC-art!"
     def checkIfExists = false
 }
