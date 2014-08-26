bb58dad6747f02d3f104269f36348216dbef2ac8
RATNASRI GURUJU <rguruju@ratnasris-mbp.gap.com>
2014-08-25 17:15:38 -0700

    ITCI-1000 [Vijay, Ratnasri] Removing major sonar issues and adding common repositories section

--- src/main/groovy/com/gap/gradle/plugins/GapTestPipelinePlugin.groovy
+++ src/main/groovy/com/gap/gradle/plugins/GapTestPipelinePlugin.groovy
@@ -3,10 +3,7 @@ package com.gap.gradle.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Zip
-import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.GradleBuild
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class GapTestPipelinePlugin implements Plugin<Project> {
 
