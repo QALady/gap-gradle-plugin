c10984b82ee5f504bd1367856c99699e90689b55
Vijay Bandari <vijay_bandari@gap.com>
2014-10-09 15:11:21 -0700

    ITCI-000 [Vijay] Configuring wrapper task to download gradle from artifactory

---
+++ src/main/groovy/com/gap/gradle/utils/Constants.groovy
@@ -0,0 +1,8 @@
+package com.gap.gradle.utils
+
+class Constants {
+
+  final ARTIFACTORY_HOME = "http://artifactory.gapinc.dev/artifactory"
+  final ARTIFACTORY_REMOTE_REPOS = "${ARTIFACTORY_HOME}/remote-repos"
+  final ARTIFACTORY_LOCAL_NON_PROD = "${ARTIFACTORY_HOME}/local-non-prod"
+}
