f902ea792ac22c8688bbb22f6633d7d1b7177c20
RATNASRI GURUJU <rguruju@RATNASRIs-MacBook-Pro.local>
2014-10-13 17:16:28 -0700

    Adding CreateHtmlWithGoodVersionsTask

---
+++ src/main/groovy/com/gap/gradle/tasks/CreateHtmlWithGoodVersionsTask.groovy
@@ -0,0 +1,28 @@
+package com.gap.gradle.tasks
+
+import org.apache.commons.logging.LogFactory
+import com.gap.pipeline.tasks.WatchmenTask
+import com.gap.pipeline.ec.CommanderClient
+import com.gap.gradle.utils.ShellCommand
+
+class CreateHtmlWithGoodVersionsTask extends WatchmenTask {
+	def logger = LogFactory.getLog(com.gap.gradle.tasks.CreateHTMLWithGoodVersionsTask)
+	ShellCommand shellCommand = new ShellCommand()
+	CommanderClient commanderClient = new CommanderClient(shellCommand)
+
+	def execute() {
+		try {
+			
+		}catch (all) {
+			logger.info("Unable to create HTML with good versions")
+			logger.debug(all.message)
+		}
+	}
+
+	def buildDependenciesHtml() {
+		def ivyDependencies = commanderClient.getECProperty("/myJob/ivyDependencies");
+		def dependenciesHtml = ""
+	}
+
+
+}
