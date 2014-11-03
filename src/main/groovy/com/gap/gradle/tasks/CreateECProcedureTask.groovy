555e5b183cb7f9f0cf5a1b1e8f2bac519ca6878a
krishnarangavajhala <krishnarangavajhala@172.17.100.135>
2014-11-03 14:54:41 -0800

    ITCI-1194 [Krishna]

--- src/main/groovy/com/gap/gradle/tasks/CreateECProcedureTask.groovy
+++ src/main/groovy/com/gap/gradle/tasks/CreateECProcedureTask.groovy
@@ -65,11 +65,11 @@ class CreateECProcedureTask extends WatchmenTask {
 		logger.info("Creating Step in ($projectName:$procedureName). stepName: '$stepName' (Delegating to: ${subProjectName}:${subProcedure})")
 
 		def ecStepConfig = [:]
-		ecStepConfig.put('condition', dsl.eCStepRunCondition)
+		ecStepConfig.put('condition', dsl.getECStepRunCondition())
 		ecStepConfig.put('subproject', subProjectName)
 		ecStepConfig.put('subprocedure', subProcedure)
-		ecStepConfig.put('parallel', dsl.eCParallelStep)
-		ecStepConfig.put('actualParameter', dsl.eCParameters)
+		ecStepConfig.put('parallel', dsl.getECParallelStep())
+		ecStepConfig.put('actualParameter', dsl.getECParameters())
 		logger.info("Step Config: " + ecStepConfig.toString())
 		commanderClient.createStep(projectName, procedureName, stepName, ecStepConfig)
 	}
