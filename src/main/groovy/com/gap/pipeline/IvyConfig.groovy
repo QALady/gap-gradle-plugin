98cc3a821e5dbbae794a1b0fb7fbcfbc02bbb171
RATNASRI GURUJU <rguruju@ratnasris-mbp.gap.com>
2015-01-26 14:39:40 -0800

    ITCI-1214 [Ratnasri] Temporary credentials till change request processed

--- src/main/groovy/com/gap/pipeline/IvyConfig.groovy
+++ src/main/groovy/com/gap/pipeline/IvyConfig.groovy
@@ -1,8 +1,11 @@
 package com.gap.pipeline
 
+import com.gap.pipeline.ec.CommanderClient
+
 class IvyConfig {
+	CommanderClient ecclient = new CommanderClient()
     def url = "http://artifactory.gapinc.dev/artifactory/local-non-prod"
-    def userName = "ec-build"
-    def password = "EC-art!"
+    def userName = "${ecclient.getArtifactoryUserName()}"
+    def password = "${ecclient.getArtifactoryPassword()}"
     def checkIfExists = false
 }
