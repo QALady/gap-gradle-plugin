cf2bd269f4d38ff51a2474b149d7f079bea94108
Adriano Bonat <adrianob@gmail.com>
2015-03-04 11:27:15 -0300

    MPL-384 [Adriano] giving better error message if no artifact to be signed is found.

---
+++ src/main/groovy/com/gap/gradle/plugins/iossigning/exceptions/ArtifactNotFoundException.java
@@ -0,0 +1,11 @@
+package com.gap.gradle.plugins.iossigning.exceptions;
+
+import com.gap.gradle.airwatch.ArtifactSpec;
+
+import static java.lang.String.format;
+
+public class ArtifactNotFoundException extends RuntimeException {
+    public ArtifactNotFoundException(ArtifactSpec artifactSpec) {
+        super(format("No artifact to be signed was found in the dependencies with these values: %s", artifactSpec.toStringWithDefinedValues()));
+    }
+}
--- src/test/groovy/com/gap/gradle/plugins/iossigning/IpaSigningTaskTest.groovy
+++ src/test/groovy/com/gap/gradle/plugins/iossigning/IpaSigningTaskTest.groovy
@@ -1,5 +1,6 @@
 package com.gap.gradle.plugins.iossigning
 
+import com.gap.gradle.plugins.iossigning.exceptions.ArtifactNotFoundException
 import com.gap.gradle.plugins.xcode.SigningIdentity
 import org.gradle.testfixtures.ProjectBuilder
 import org.hamcrest.Matchers
@@ -8,6 +9,7 @@ import org.junit.Test
 
 import static org.hamcrest.Matchers.instanceOf
 import static org.junit.Assert.assertThat
+import static org.junit.Assert.fail
 
 class IpaSigningTaskTest {
     private project
@@ -20,7 +22,7 @@ class IpaSigningTaskTest {
 
     @Test
     public void shouldSkipTaskIfRequiredParamsWereNotSpecified() throws Exception {
-        def task = project.tasks['ipaSigning']
+        def task = project.ipaSigning
         task.execute()
 
         assertThat(task.state.skipped, Matchers.is(true))
@@ -30,5 +32,21 @@ class IpaSigningTaskTest {
     public void shouldHavePreConfiguredSigningIdentities() throws Exception {
         assertThat(project.ipaSigning.signing.development, instanceOf(SigningIdentity))
         assertThat(project.ipaSigning.signing.distribution, instanceOf(SigningIdentity))
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNoArtifactWasFoundToBeSigned() throws Exception {
+        project.configurations.create('archives')
+        project.ipaSigning {
+            signingIdentity signing.development
+            artifact.classifier 'foo'
+        }
+
+        try {
+            project.ipaSigning.execute()
+            fail("Should throw exception because there are no artifacts to be signed.")
+        } catch (e) {
+            assertThat(e.cause, instanceOf(ArtifactNotFoundException))
+        }
     }
 }
