16ed212c8148535a6ba195833c9fba20f677fd73
MRoowal <mohsin_roowalla@gap.com>
2015-03-11 08:36:14 -0700

    Removing unused imports

--- src/main/groovy/com/gap/gradle/tasks/CreateEasyCloudResourceTask.groovy
+++ src/main/groovy/com/gap/gradle/tasks/CreateEasyCloudResourceTask.groovy
@@ -20,11 +20,9 @@ import groovy.json.JsonSlurper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
