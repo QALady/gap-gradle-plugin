0dfbccc7de9e1923206fa7616c89e08a7ef2ea08
Ravi Elaprolu <ravi_elaprolu@gap.com>
2015-08-24 15:43:08 -0700

    ISDM-72 [Ravi] - Fixed issue with old simulator path

--- src/main/groovy/com/gap/gradle/plugins/appium/CopyTrustStore.groovy
+++ src/main/groovy/com/gap/gradle/plugins/appium/CopyTrustStore.groovy
@@ -7,14 +7,15 @@ import org.gradle.api.tasks.TaskAction
 import static java.lang.System.getProperty
 
 class CopyTrustStore extends DefaultTask {
-    private static final String IOS7_KEYCHAIN_BASE_DIR = "${getProperty('user.home')}/Library/Application Support/iPhone Simulator"
+//    private static final String IOS7_KEYCHAIN_BASE_DIR = "${getProperty('user.home')}/Library/Application Support/iPhone Simulator"
     private static final String IOS8_KEYCHAIN_BASE_DIR = "${getProperty('user.home')}/Library/Developer/CoreSimulator/Devices"
 
     String trustStoreFile
 
     @TaskAction
     run() {
-        keychainPathsFor(IOS7_KEYCHAIN_BASE_DIR, IOS8_KEYCHAIN_BASE_DIR).each { File destination ->
+//        keychainPathsFor(IOS7_KEYCHAIN_BASE_DIR, IOS8_KEYCHAIN_BASE_DIR).each { File destination ->
+        keychainPathsFor(IOS8_KEYCHAIN_BASE_DIR).each { File destination ->
             project.logger.info "Copying \"${trustStoreFile}\" into \"$destination\""
 
             project.copy {
